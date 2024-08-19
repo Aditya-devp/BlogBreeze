759ef66fd2731131e9648a6787c38e899abf4bf0
Aditya Singh <singhaditya6774@gmail.com>
2024-08-19 15:52:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,29 +4,18 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import Connection from "./database/db.js";
 import Router from "./routes/route.js";
-import path from "path";
+
 
 dotenv.config();
 
-const __dirname = path.resolve();
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", Router);
-app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
 
 const PORT = process.env.PORT || 8000;
 
